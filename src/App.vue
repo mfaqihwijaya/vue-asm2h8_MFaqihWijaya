9cb5d4d6a9b8a385c42e2cba9b6a7f004a249b9c
mfaqihwijaya <faqih.wijaya@bithealth.co.id>
2024-02-20 23:56:17 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>TODO APP</h1>
+  <div class="container">
+    <TodoBoard
+      :todos="todos"
+      @addTodo="addTodo"
+      @deleteTodo="deleteTodo"
+      @changeStatusTodo="changeStatusTodo"
+    />
+  </div>
+</template>
+
+<script setup>
+import TodoBoard from './components/TodoBoard.vue';
+import { ref } from 'vue';
+const todos = ref([]);
+function addTodo(newTodo) {
+  todos.value.push(newTodo);
+}
+function deleteTodo(todoId) {
+  todos.value = todos.value.filter((todo) => todo.id !== todoId);
+}
+function changeStatusTodo(todoId) {
+  const todoIndex = todos.value.findIndex((todo) => todo.id === todoId);
+  todos.value[todoIndex].completed = !todos.value[todoIndex].completed;
+}
+</script>
+
+<style scoped>
+.container {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 30px;
+}
+
+h1 {
+  margin: 32px;
+  text-align: center;
+  color: #f8b7cd;
+}
+</style>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,39 @@
+<template>
+  <ol>
+    <li v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" @change="changeStatus(todo.id)" />
+      <span :class="{ completed: todo.completed }"
+        ><strong class="textTodo">{{ todo.text }}</strong></span
+      >
+      <button @click="deleteThis(todo.id)" id="delete">Delete</button>
+      <br />
+      <span>Today:</span>
+      <span>{{ todo.createdWhen }}</span>
+      <br />
+      <span>Deadline Date:</span>
+      <span>{{ todo.deadline }}</span>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'TodoItems',
+  props: ['todos'],
+  methods: {
+    deleteThis(todoId) {
+      this.$emit('deleteTodo', todoId);
+    },
+    changeStatus(todoId) {
+      this.$emit('changeStatusTodo', todoId);
+    },
+  },
+};
+</script>
+
+<style>
+#delete {
+  color: black;
+  background: #f8b7cd;
+}
+</style>
